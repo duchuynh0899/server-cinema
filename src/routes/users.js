66e113f808832c65ad4ebd81eb5e1c7fcbfcd039
duchuynh0899 <duchuynh0899@gmail.com>
2021-05-27 23:56:06 +0700

    oke

--- src/routes/users.js
+++ src/routes/users.js
@@ -194,7 +194,7 @@ router.patch('/users/me', auth.simple, async (req, res) => {
   }
 });
 
-// Admin can update user by id
+// Admin can update user by id đâsd
 router.patch('/users/:id', auth.enhance, async (req, res) => {
   if (req.user.role !== 'superadmin')
     return res.status(400).send({
