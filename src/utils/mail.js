55dc7fe1d34eb79821bdf198b303ac15b19cf438
duchuynh0899 <duchuynh0899@gmail.com>
2021-06-28 21:38:05 +0700

    oke

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,23 +1,23 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  // service: 'smtp.gmail.com', //smtp.gmail.com  //in place of service use host...
-  // secure: true, //true
-  // port: 465, //465
-  // auth: {
-  //   user: 'duchuynh0899@gmail.com',
-  //   pass: 'Duchuynh123',
-  // },
-  // tls: {
-  //   rejectUnauthorized: false,
-  // },
-  host: 'https://server-cinema.herokuapp.com',
-  port: 465,
-  secure: true,
+  service: 'smtp.gmail.com', //smtp.gmail.com  //in place of service use host...
+  secure: true, //true
+  port: 465, //465
   auth: {
     user: 'duchuynh0899@gmail.com',
-    pass: 'Duchuynh123'
-  }
+    pass: 'Duchuynh123',
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  // host: 'smtp.gmail.com',
+  // port: 465,
+  // secure: true,
+  // auth: {
+  //   user: 'duchuynh0899@gmail.com',
+  //   pass: 'Duchuynh123'
+  // }
 });
 
 transporter.sendEMail = function(mailRequest) {
